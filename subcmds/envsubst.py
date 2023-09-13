86546fc3a404f867ebd292f3cd18c6f6df6f2240
Phil M <phil.m@nttdata.com>
2023-09-13 15:46:51 -0400

    feat: dso-1186. Extend 'envsubst' subcommand. Unit tests added
    - Intention is Version control multi components living in multi repos
    - By keeping XML manifest for them in a <super repository>/manifests/entry.xml
    - Versioned per se by git tag on <super repository>
    - allow engineer override version of any component(s)
    - ``` <project name='xyz' dso_override_attrbiute_revision='${XYZ_VER}'>```
    - results after `XYZ_VER=1.2.3 repo envsubst` in ``` <project name='xyz' revision='1.2.3'>
    - Note - 3 unitests from 228 fails - that's NOT Launch patch code but baseline one

--- subcmds/envsubst.py
+++ subcmds/envsubst.py
@@ -31,6 +31,7 @@ Replace ENV vars in all xml manifest files
 
 Finds all XML files in the manifests and replaces environment variables with values.
 """
+    path = '.repo/manifests/**/*.xml'
 
     # def _Options(self, p):
     #     p.add_option(
@@ -59,41 +60,94 @@ Finds all XML files in the manifests and replaces environment variables with val
             args: Positional args.  Can be a list of projects to list, or empty.
         """
         print(f"Executing envsubst {opt}, {args}")
-        path = '.repo/manifests/**/*.xml'
-        files = glob.glob(path, recursive=True)
+        files = glob.glob(self.path, recursive=True)
 
         for file in files:
             print(file)
             if os.path.getsize(file) > 0:
                 self.EnvSubst(file)
 
-
     def EnvSubst(self, infile):
         doc = minidom.parse(infile)
+        self.search_replace_placeholders(doc)
+        self.inject_attrbutes(doc)
+        # backup file before we modified it - for troubleshooting
+        os.rename(infile, infile + '.bak')
+        self.save(infile,doc)
 
-        # Replace environment variables in all element texts and attribute values
-        for elem in doc.getElementsByTagName('*'):
-            for key, value in elem.attributes.items():
-                # Check if the attribute value contains an environment variable
-                if '$' in value:
-                    # Replace the environment variable with its value
-                    elem.setAttribute(key, os.path.expandvars(value))
-            if elem.firstChild and elem.firstChild.nodeType == elem.TEXT_NODE and '$' in elem.firstChild.nodeValue:
-                # Replace the environment variable with its value
-                elem.firstChild.nodeValue = os.path.expandvars(
-                    elem.firstChild.nodeValue)
-
-        pretty_print = lambda data: '\n'.join([
+    def save(self,outfile,doc):
+        """
+        Save the modified XML document with comments and the XML header
+        """
+        def pretty_print(data): return '\n'.join([
             line
             for line in parseString(data).toprettyxml(indent=' ' * 2).split('\n')
             if line.strip()
         ])
-
-        # Save the modified XML document with comments and the XML header
-        os.rename(infile, infile + '.bak')
-
-        with open(infile, 'wb') as f:
+        with open(outfile, 'wb') as f:
             f.write(str.encode(pretty_print(doc.toprettyxml(encoding="utf-8"))))
-            #f.write(doc.toprettyxml(encoding="utf-8"))
         f.writelines
         f.close
+
+    def search_replace_placeholders(self, doc):
+        """
+        Replace in all element texts and attribute values any ${PLACEHOLDER} by value as returned from #resolveVariable
+        """
+        for elem in doc.getElementsByTagName('*'):
+            for key, value in elem.attributes.items():
+                # Check if the attribute value contains an environment variable
+                if self.is_placeholder_detected(value):
+                    # Replace the environment variable with its value
+                    elem.setAttribute(key, self.resolve_variable(value))
+            if elem.firstChild and elem.firstChild.nodeType == elem.TEXT_NODE and self.is_placeholder_detected(elem.firstChild.nodeValue):
+                # Replace the environment variable with its value
+                elem.firstChild.nodeValue = self.resolve_variable(
+                    elem.firstChild.nodeValue)
+
+    def is_placeholder_detected(self, value):
+        return '$' in value
+
+    def resolve_variable(self, var_name):
+        """
+        resolves variables from OS env vars
+        """
+        return os.path.expandvars(var_name)
+
+    def inject_attrbutes(self, doc):
+        """
+        When encounters XML element <any_element attrname='any value' dso_override_attrbiute_attrname="replacement">
+        or <any_element  dso_override_attrbiute_attrname="replacement">
+        Replace it by <any_element attrname="replacement">
+        """
+        for elem in doc.getElementsByTagName('*'):
+            while self.attr_2_override_detected(elem):
+                override_info = self.get_next_attr_override(elem)
+                self.inject_or_replace(elem, override_info[0], override_info[1])
+                elem.removeAttribute(override_info[2])
+
+    def attr_2_override_detected(self, elem) -> bool:
+        for key, value in elem.attributes.items():
+            if key.startswith('dso_override_attrbiute_'):
+                return True
+        return False
+
+    def get_next_attr_override(self, node) -> [str, str, str]:
+        """
+        when node is <any_element attrname='any value' dso_override_attrbiute_attrname="replacement">
+        returns ['attrname','replacement','dso_override_attrbiute_attrname'] 
+        otherwise returns an empty array
+        """
+        for key, value in node.attributes.items():
+            if key.startswith('dso_override_attrbiute_'):
+                attr_2_override = key[len('dso_override_attrbiute_'):]
+                attr_2_override_value = value
+                attr_2_delete = key
+                return [attr_2_override, attr_2_override_value, attr_2_delete]
+        return []
+
+    def inject_or_replace(self, elem, attr_name, attr_value) -> None:
+        if self.is_placeholder_detected(attr_value):
+            return
+        if elem.hasAttribute(attr_name):
+            elem.removeAttribute(attr_name)
+        elem.setAttribute(attr_name, attr_value)
